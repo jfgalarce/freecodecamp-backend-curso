7720a80c13d25f79a3e2a45c8d8ddd0acae14a60
juan galarce <jfgalarce@gmail.com>
2025-02-01 17:29:16 +0000

    test 1 finalizado

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,42 +20,42 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hellos", function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/:date?",function(req, res){
-  const dateString = req.params.date;
-  let response = {};
 
-  // If no date is provided, use the current date
-  if (!dateString) {
-    const now = new Date();
-    response.unix = now.getTime();
-    response.utc = now.toUTCString();
-  } else {
-    let date;
-    // If the date is a valid Unix timestamp
-    if (!isNaN(dateString)) {
-      date = new Date(parseInt(dateString));
-    } 
-    // If it's a valid date string
-    else {
-      date = new Date(dateString);
-    }
+app.get('/api', (req, res) => {
+  const date = new Date();
 
-    // If the date is invalid, return an error message
-    if (date.toString() === "Invalid Date") {
-      return res.json({ error: "Invalid Date" });
-    }
+  res.json({
+    "unix": date.getTime(),
+    "utc": date.toUTCString()
+  });
+})
 
-    // If the date is valid, return both Unix and UTC
-    response.unix = date.getTime();
-    response.utc = date.toUTCString();
+
+app.get('/api/:date', (req, res) => {
+  const { date } = req.params;
+  let dateTime = '';
+  const regex = /^\d+$/;
+  if (regex.test(date)) {
+    dateTime = new Date(parseInt(date));
+  }else{
+    dateTime = new Date(date);
+  }
+  
+  if (isNaN(dateTime.getTime())) {
+    return res.json({ error: "Invalid Date" });
   }
 
-  res.json(response);
+  res.json({
+    "unix": dateTime.getTime(),
+    "utc": dateTime.toUTCString()
+  });
 })
+
+
 
 
 // Listen on port set in environment variable or default to 3000
